9825ffe312cf94f4b914add7ae44f8a14917e602
liuxin <liuxin85@gmail.com>
2023-07-27 23:15:40 +0800

    redict to dashboard page

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -13,7 +13,7 @@ const SetupPage = () => {
     }
   }, [isOpen, onOpen]);
 
-  return <div className="p-4">root page</div>;
+  return null;
 };
 
 export default SetupPage;
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -43,7 +43,8 @@ export const StoreModal = () => {
       // throw new Error("X")
       const response = await axios.post("/api/stores", values);
       // console.log(response.data);
-      toast.success("Store created.");
+      // toast.success("Store created.");
+      window.location.assign(`/${response.data.id}`)
     } catch (error) {
       // console.log(error);
       toast.error("Something weng wrong");
